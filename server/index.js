2e8e13a2b2401caa48d133c1a8f91979c9d3dd50
TheoAt <theo.atlani@gmail.com>
2023-06-19 01:09:51 +0200

    try remove node_modules

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
-app.get('/', async (req, res) => {
+app.get ('/', async (req, res) => {
   res.send('Hello from DALL-E!')
 })
 
